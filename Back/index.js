9c38c9d39c2c9cdf3e4a6c030ed2f707c068e3b0
Quentin-J-Sphinx <quentin.janowszik@gmail.com>
2023-08-11 14:10:13 +0200

    Test

--- Back/index.js
+++ Back/index.js
@@ -3,7 +3,7 @@ import session from 'express-session';
 import * as dotenv from 'dotenv';
 import router from './app/router.js';
 import cors from 'cors';
-
+import * as path from 'path';
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static('uploads'));
-
+app.use(express.static(path.resolve(__dirname, '../Front/dist')));
 app.use(express.json());
 
 app.use(cors());
